74236868184d0aa35f5b4f400d68fbfc396513d4
akil1998 <30828660+akil1998@users.noreply.github.com>
2017-08-08 15:51:49 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,20 @@
+# AKIL
+import java.io.*;
+import java.util.*;
+class num
+{
+public static void main(String args[])
+int a=10
+if(a>0)
+{
+system.out.println("the given value is positive");
+}
+else if(a=0)
+{
+system.out.println("the given value is zero");
+}
+else
+{
+system.out.println("the given number is negative");
+}
+}
